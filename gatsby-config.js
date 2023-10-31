5fe7e5ac89fe2edd100389f9c83c61a607fb559b
farhanzip <farhanaji317@gmail.com>
2023-10-31 15:50:11 +0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-blogger',
+      options: {
+        apiKey: 'AIzaSyBxIvXPj6cbkqMXTK0gDV9MSS5VdgVKoO4',
+        blogId: '1626104544365850862'
+        }
+    }
+  ],
+}
