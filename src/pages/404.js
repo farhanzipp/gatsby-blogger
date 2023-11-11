1ca658666fa9ce47c2b8b8fcc30a8f8e6d5cee5c
farhanzip <farhanaji317@gmail.com>
2023-11-11 14:28:13 +0700

    feat: styling 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,21 @@ import PageLayout from '../components/PageLayout';
 function PageNotFound() {
   return (
     <PageLayout>
-      <div>PageNotFound</div>
+      <div className="text-center">
+        <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
+        <p className="mb-4 text-lg text-gray-600">Oops! Looks like you are lost.</p>
+        <div className="animate-bounce">
+          <svg className="mx-auto h-16 w-16 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+          </svg>
+        </div>
+        <p className="mt-4 text-gray-600">
+          Go Home?
+
+          <a href="/" className="text-blue-500">home</a>
+          .
+        </p>
+      </div>
     </PageLayout>
   );
 }
